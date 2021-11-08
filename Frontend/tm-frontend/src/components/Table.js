79e62dcef5cd66ecf1ce2ecc9f9743d30a5bbeb4
Prem <macpro@MacPros-Mac-Pro.local>
2021-11-08 15:57:04 -0500

    Initial dummy data in table

---
+++ Frontend/tm-frontend/src/components/Table.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(packageName, packageURL, rating) {
+  return { packageName, packageURL, rating };
+}
+
+const rows = [
+  createData("Lodash", "https://github.com/lodash/lodash", 0.85),
+  createData("Lodash", "https://github.com/lodash/lodash", 0.85),
+  createData("Lodash", "https://github.com/lodash/lodash", 0.85),
+  createData("Lodash", "https://github.com/lodash/lodash", 0.85),
+  createData("Lodash", "https://github.com/lodash/lodash", 0.85),
+];
+
+export default function Header() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Package Name</TableCell>
+            <TableCell align="left">Package URL</TableCell>
+            <TableCell align="right">Overall Rating</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.packageName}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.packageName}
+              </TableCell>
+              <TableCell align="left">{row.packageURL}</TableCell>
+              <TableCell align="right">{row.rating}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
