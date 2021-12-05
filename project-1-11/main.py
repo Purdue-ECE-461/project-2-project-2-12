51f7bc55f2aadcd69a80138c89acb231f3305539
estheralee <elee5232@gmail.com>
2021-12-04 23:24:43 -0500

    changed main.py to be a class

--- project-1-11/main.py
+++ project-1-11/main.py
@@ -8,48 +8,23 @@ from bs4 import BeautifulSoup
 import urllib
 import urllib.request
 
+class Package:
+    def __init__(self, url):
+        self.url = url
+        self.total_score = self.getTotalScore(url)
 
-def getNpmRepo(url):
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
-    git_url_box = soup.find('a', attrs={'class': 'b2812e30 f2874b88 '
-                                                 'fw6 mb3 mt2 truncate black-80 f4 link'})
-    git_url = 'https://' + git_url_box.text[3:]
+    def getTotalScore(self, url):
+        printStrings = {}
+        npm_flag = 'www.npmjs.com'
+        line_copy = url
+        if npm_flag in url:
+            owner, module = self.urlParse(url)
+            line_copy = self.getNpmRepo(url)
+        owner, module = self.urlParse(line_copy)
 
-    """ This is the original code """
-    # response = requests.get(git_url).json()
-    # print(response["results"][0]["package"]["links"])
-    # git_url = response["results"][0]["package"]["links"]["repository"]
-    return git_url
-
-
-def urlParse(url):
-    parse = urlparse(url)  # Parses URL to specific components
-    path = parse[2]  # Variable for storing path (ex: /lodash/lodash)
-    ownerModule = re.split(r'/', path)
-    owner = ownerModule[1]
-
-    module = ownerModule[2].strip('\n')
-    return owner, module
-
-def main(input):
-    npm_flag = 'www.npmjs.com'
-    printOrder = []
-    printStrings = {}
-    levels = {'0': logging.CRITICAL, '1': logging.INFO, '2': logging.DEBUG}
-    logging.basicConfig(filename=os.getenv('LOG_FILE'), level=levels[os.getenv('LOG_LEVEL')], filemode="w")
-
-    with open(input) as f:
-        lines = f.readlines()
-    for line in lines:
-        line_copy = line
-        if npm_flag in line:
-            owner, module = urlParse(line)
-            line_copy = getNpmRepo(line)
-        owner, module = urlParse(line_copy)
-
-        logging.info(f"URL: {line}")
+        logging.info(f"URL: {url}")
         logging.info(f"Repository Owner: {owner} " + f" Module: {module}")
+
         # Change the owner and module name to corresponding names to test for different repo
         test_response = getRepo(owner, module)
         if 'message' in test_response:
@@ -65,23 +40,38 @@ def main(input):
         depend_score = getDependencyScore(owner, module)
         total_score = getTotalScore(rsp_score, rmp_score, cor_score, bus_score, license_score, depend_score)
         logging.info("Metric scores succesfully calculated.")
-        line = line.strip("\n")
-        printOrder.append(total_score)
-        printStrings[line] = f"{total_score}"
-        # printStrings[
-        #     total_score] = f"{line} {round(total_score, 2)} {round(rmp_score, 2)} {round(cor_score, 2)} {round(bus_score, 2)} {round(rsp_score, 2)} {round(license_score, 2)} {round(depend_score, 2)}"
+        printStrings[url] = f"{total_score}"
+        return json.dumps(printStrings)
 
-    return json.dumps(printStrings)
+    def getNpmRepo(self, url):
+        page = urllib.request.urlopen(url)
+        soup = BeautifulSoup(page, 'html.parser')
+        git_url_box = soup.find('a', attrs={'class': 'b2812e30 f2874b88 '
+                                                     'fw6 mb3 mt2 truncate black-80 f4 link'})
+        git_url = 'https://' + git_url_box.text[3:]
+
+        """ This is the original code """
+        # response = requests.get(git_url).json()
+        # print(response["results"][0]["package"]["links"])
+        # git_url = response["results"][0]["package"]["links"]["repository"]
+        return git_url
+
+    def urlParse(self, url):
+        parse = urlparse(url)  # Parses URL to specific components
+        path = parse[2]  # Variable for storing path (ex: /lodash/lodash)
+        ownerModule = re.split(r'/', path)
+        owner = ownerModule[1]
+
+        module = ownerModule[2].strip('\n')
+        return owner, module
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        logging.info("DEBUG - Wrong number of inputs!")
-        logging.info("System exiting...")
-        sys.exit(1)
-    json = main(sys.argv[1])
-    print(json)
-    # print(printStrings)
-    # printOrder.sort()
-    # for i in printOrder[::-1]:
-    #     print(printStrings[i])
+    # pkg = Package('https://github.com/lodash/lodash')
+    # json = pkg.total_score
+    # print(json)
+
+    pkg2 = Package('https://www.npmjs.com/package/browserify')
+    json2 = pkg2.total_score
+    print(json2)
+
